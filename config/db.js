dee55112bc43a08179f32aba58cfacedc1909cbe
deepti2820 <165156846+deepti2820@users.noreply.github.com>
2024-09-09 00:53:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 module.exports.connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://deeptisinghal2003:MkzLzwx23u63azDV@cluster0.igbmd03.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect(process.env.DATABASE)
     .then(()=>{
         console.log("DB Connected")
     })
